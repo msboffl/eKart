2b65ec501f4c1eff59b0cd05ee4be90b0e12da82
Mareedu Saibabu <a3097640@bestbuy.com>
2025-10-24 09:38:08 +0530

    web-store

---
+++ apps/web-store/src/app/signup/page.tsx
@@ -0,0 +1,10 @@
+export default function SignUp() {
+  return (
+    <>
+      <div className="flex flex-row p-4">
+        <div className="basis-1/3">01</div>
+        <div className="basis-2/3 bg-linear-to-r from-[#E7F0FD] to-[#ACCBEE]">02</div>
+      </div>
+    </>
+  );
+}
